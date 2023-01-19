c70eb4ee368176fd4bed96935a39772fda54f53e
chall <christopher.hall@trademe.co.nz>
2023-01-20 08:42:47 +1300

    DI implementation update

--- app/src/main/java/com/example/inventory/MainActivity.kt
+++ app/src/main/java/com/example/inventory/MainActivity.kt
@@ -19,7 +19,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.inventory.ui.theme.InventoryTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
